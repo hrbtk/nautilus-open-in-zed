5d3f10a0eefff9e7dcd1646d03dc7a9acd615527
hrbtk <viktor.beskorovainyi@gmail.com>
2025-06-28 22:43:09 +0200

    Added initial changes for Zed

--- nautilus-open-in-zed.py
+++ nautilus-open-in-zed.py
@@ -2,7 +2,7 @@ from gi.repository import Nautilus, GObject
 from typing import List
 
 
-class OpenInVSCodeExtension(GObject.GObject, Nautilus.MenuProvider):
+class OpenInZedEditorExtention(GObject.GObject, Nautilus.MenuProvider):
     def get_file_items(
         self,
         files: List[Nautilus.FileInfo],
@@ -21,26 +21,26 @@ class OpenInVSCodeExtension(GObject.GObject, Nautilus.MenuProvider):
         is_background: bool,
     ) -> List[Nautilus.MenuItem]:
         menu_item = Nautilus.MenuItem(
-            name="OpenInVSCodeExtension::OpenInVSCode" + "Background" if is_background else "",
-            label="Open in VSCode",
-            tip="Open the selected files in Visual Studio Code.",
+            name="OpenInZedEditorExtention::OpenInZed" + "Background" if is_background else "",
+            label="Open in Zed",
+            tip="Open the selected files in Zed Editor.",
             icon="",
         )
         menu_item.connect(
             "activate",
-            lambda menu_item, files=files: self.open_in_vscode(files),
+            lambda menu_item, files=files: self.open_in_zed(files),
         )
         return [
             menu_item,
         ]
 
-    def open_in_vscode(self, files: List[Nautilus.FileInfo]) -> None:
+    def open_in_zed(self, files: List[Nautilus.FileInfo]) -> None:
         import subprocess
         if len(files) == 1 and files[0].is_directory():
-            # Open folder in VSCode if only one directory is selected
+            # Open folder in Zed if only one directory is selected
             folder_path = files[0].get_location().get_path()
-            subprocess.run(["code", "-n", folder_path])
+            subprocess.run(["zed", "-n", folder_path])
         else:
-            # Open files in VSCode if multiple files are selected, disregarding directories
+            # Open files in Zed if multiple files are selected, disregarding directories
             file_paths = [file.get_location().get_path() for file in files if not file.is_directory()]
-            subprocess.run(["code"] + file_paths)
+            subprocess.run(["zed"] + file_paths)
